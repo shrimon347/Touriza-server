9196aab0d88df35f7a0472dd3d17eb1ba88e4307
shrimon347 <shrimon1999@gamil.com>
2024-05-17 12:04:26 +0600

    link updated origin added

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ const uri = `mongodb+srv://shrimon1999:${process.env.DB_PASS}@cluster0.ppfmuai.m
 // console.log(uri);
 
 //middleware
-app.use(cors());
+const corsConfig = {
+  origin: ["http://localhost:5173",'http://localhost:5174',"https://brandshop-82def.web.app"],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
+app.use(cors(corsConfig));
+app.options("", cors(corsConfig));
 app.use(express.json());
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,19 +31,18 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const tourCollection = client.db("Touriza").collection("TouristSpot");
     const countryCollection = client.db("Touriza").collection("Country");
     const userCollection = client.db("Touriza").collection("User");
     app.get("/tour", async (req, res) => {
-        
       let query = {};
       // console.log(req.query);
-        if(req.query?.email){
-            query = {email : req.query.email}
-        }
-      
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+
       const result = await tourCollection.find(query).toArray();
       res.send(result);
     });
@@ -48,23 +53,23 @@ async function run() {
       res.send(result);
     });
     app.get("/country/:name", async (req, res) => {
-      const name = req.params.name
-      const query = {countryName : name}
+      const name = req.params.name;
+      const query = { countryName: name };
       console.log(name);
-      const result= await countryCollection.findOne(query);
+      const result = await countryCollection.findOne(query);
       // const result = await cursor.toArray();
       res.send(result);
     });
     app.post("/tour", async (req, res) => {
       const newTour = req.body;
-    //   console.log(newTour);
+      //   console.log(newTour);
       const result = await tourCollection.insertOne(newTour);
       console.log(result);
       res.send(result);
     });
     app.post("/user", async (req, res) => {
       const newUser = req.body;
-    //   console.log(newTour);
+      //   console.log(newTour);
       const result = await userCollection.insertOne(newUser);
       console.log(result);
       res.send(result);
@@ -87,16 +92,16 @@ async function run() {
       const updatedTour = req.body;
       const tour = {
         $set: {
-          name : updatedTour.name,
-          tourisSpotName : updatedTour.tourisSpotName, 
-          countryName : updatedTour.countryName,
-           location : updatedTour.location, 
-           averageCost : updatedTour.averageCost, 
-           travelTime : updatedTour.travelTime, 
-           totalVisitorPerYear: updatedTour.totalVisitorPerYear, 
-           seasonality : updatedTour.seasonality, 
-           image: updatedTour.image, 
-           description : updatedTour.description 
+          name: updatedTour.name,
+          tourisSpotName: updatedTour.tourisSpotName,
+          countryName: updatedTour.countryName,
+          location: updatedTour.location,
+          averageCost: updatedTour.averageCost,
+          travelTime: updatedTour.travelTime,
+          totalVisitorPerYear: updatedTour.totalVisitorPerYear,
+          seasonality: updatedTour.seasonality,
+          image: updatedTour.image,
+          description: updatedTour.description,
         },
       };
       const result = await tourCollection.updateOne(filter, tour, option);
@@ -119,10 +124,10 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+  //  await client.db("admin").command({ ping: 1 });
+  //   console.log(
+  //     "Pinged your deployment. You successfully connected to MongoDB!"
+  //   ); 
   } finally {
     // Ensures that the client will close when you finish/error
     //await client.close();
